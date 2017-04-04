ddda123f23a87eb151d22205f75f1f320ce2a2ea
Thomas Johnson <tjohnson061944@gmail.com>
2017-04-04 00:19:45 -0500

    Added some cool new functions to use

---
+++ src/display.py
@@ -0,0 +1,13 @@
+from sense_hat import SenseHat
+
+    def displayRise(senseName):
+        senseName.load_image('file path here')
+
+    def displayDrop(senseName):
+        senseName.load_image('file path here')
+
+    def displayClouds(senseName):
+        senseName.load_image('file path here')
+
+    def displaySun(senseName):
+        sensename.load_image('file path here')
