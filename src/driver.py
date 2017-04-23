786b00eaca44cfb6a01a830bfadb178a7829dfe2
Thomas Johnson <tjohnson061944@gmail.com>
2017-04-23 13:09:18 -0500

    Converted Pressure Unit, Commented Code, Ready For Submission

--- src/driver.py
+++ src/driver.py
@@ -36,45 +36,59 @@ riseOrDrop = 0
 loopCount = 0
 lastTemp = 0
 url = 'http://52.33.110.204/node/weather/api/stationdata'
-stationid = 1
+stationid = 1 #This is the unique ID for my personal station.
 
 ############### "Main" Execution ####################################
 
 while True:
-
+    ##Clears the display so that it is not always on.##
     off(sense)
 
+    ##Reads the temperature from two different sensors##
+    ##Also reads the temperature from the CPU##
     temperature1 = sense.get_temperature_from_humidity()
     temperature2 = sense.get_temperature_from_pressure()
     machineTemp = cpuTemp()
 
+    ##Takes the average of the sensor reads##
+    ##Accounts for the CPU temperature##
+    ##Converts to Farenheit because I don't get Celsius##
     temp = (temperature1+temperature2)/2
     accountForCPU = temp - ((machineTemp - temp)/1.5)
     temp = c_to_f(accountForCPU)
 
+    ##Adds the current temperature read to a list of 5 temperatures##
+    ##Removes the oldest temperature in memory##
     tempList.append(temp)
     tempList.pop(0)
 
+    ##Checks to see how many times the program has looped##
     loopCount = loopCount % 5
 
+    ##If it is the fifth time the program has looped then we enter##
     if(loopCount == 0):
+        ##Takes the average of the last 5 temperature reads##
 	    avgTemp = 0
         for nums in tempList:
             avgTemp += nums
         avgTemp = avgTemp/len(tempList)
 
+        ##Subtracts the current average of the 5 reads with the previous##
         riseOrDrop = lastTemp - avgTemp
         lastTemp = avgTemp
 
+        ##If riseOrDrop is positive I.E. the previous average was greater than then current##
+        ##the LED matrix displays a drop in temperature or vice versa##
         if(riseOrDrop > 0):
             displayDrop(sense)
         else:
             displayRise(sense)
 
+    ##Reads the pressure and humidity sensors##
+    pressure = sense.get_pressure()*.02953 #In Hg
+    humidity = sense.get_humidity() #In %rH
 
-    pressure = sense.get_pressure()
-    humidity = sense.get_humidity()
-
+    ##Creates a HTTP Post payload with data##
     payload = {
         'station_id': stationid,
         'temperature': temp,
@@ -82,6 +96,8 @@ while True:
         'pressure': pressure
     }
 
+    ##Posts the data to our web server.
     r = requests.post(url, data=payload)
     loopCount += 1
+    ##Waits 10 seconds before reading again##
     time.sleep(10)
